0e618add8d1ea3b7d94ee5c518977f1142180d0d
Thiago <thiagoroberto2003@gmail.com>
2021-03-06 20:44:20 -0300

    Added a new file to do the tasks functions and chage a bit html

--- main.js
+++ main.js
@@ -7,26 +7,89 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("const btn = document.querySelector(\"#btnAddTask\");\nconst divTodos = document.querySelector('.todos');\n\ncriaTarefa = () => {\n\n    let li = document.createElement('li');\n    let divChecked = document.createElement('div');\n    let spanName = document.createElement('span');\n    let div = document.createElement('div');\n    let btnEdit = document.createElement('button');\n    let btnTrash = document.createElement('button');\n    let iCircle = document.createElement('i');\n    let iPencil = document.createElement('i');\n    let iTrash = document.createElement('i');\n\n    li.appendChild(divChecked);\n    divChecked.appendChild(btnEdit);\n    li.appendChild(spanName);\n    li.appendChild(div);\n    div.appendChild(btnEdit);\n    div.appendChild(btnTrash);\n\n    divChecked.classList.add('checked');\n    \n\n    iCircle.classList.add('fa', 'fa-circle-o');\n    iPencil.classList.add('fa','fa-pencil');\n    iTrash.classList.add('fa','fa-trash');\n\n    divChecked.appendChild(iCircle);\n    btnEdit.appendChild(iPencil);\n    btnTrash.appendChild(iTrash);\n\n\n    return li;\n}\n\n\n\nbtn.addEventListener('click', ()=> divTodos.append(criaTarefa()));\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_task_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/task.js */ \"./src/modules/task.js\");\n\n\n\n\n\nconst btn = document.querySelector(\"#btnAddTask\");\nconst divTodos = document.querySelector('.todos');\nconst modal = document.querySelector('.modal');\n\n\nbtn.addEventListener('click', ()=> {\n    //modal.style.display = 'block';\n    divTodos.append((0,_modules_task_js__WEBPACK_IMPORTED_MODULE_0__.default)());\n});\n\n\n//# sourceURL=webpack://to-do-list/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modules/task.js":
+/*!*****************************!*\
+  !*** ./src/modules/task.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction criaTarefa (){\n\n    let li = document.createElement('li');\n    let divChecked = document.createElement('div');\n    let spanName = document.createElement('span');\n    let div = document.createElement('div');\n    let btnEdit = document.createElement('button');\n    let btnTrash = document.createElement('button');\n    let iCircle = document.createElement('i');\n    let iPencil = document.createElement('i');\n    let iTrash = document.createElement('i');\n\n    li.appendChild(divChecked);\n    divChecked.appendChild(btnEdit);\n    li.appendChild(spanName);\n    li.appendChild(div);\n    div.appendChild(btnEdit);\n    div.appendChild(btnTrash);\n\n    divChecked.classList.add('checked');\n    \n\n    iCircle.classList.add('fa', 'fa-circle-o');\n    iPencil.classList.add('fa','fa-pencil');\n    iTrash.classList.add('fa','fa-trash');\n\n    divChecked.appendChild(iCircle);\n    btnEdit.appendChild(iPencil);\n    btnTrash.appendChild(iTrash);\n\n    return li;  \n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (criaTarefa);\n\n//# sourceURL=webpack://to-do-list/./src/modules/task.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
