1e21cad099c99420f31443fec12641840a2a2905
Thiago <thiagoroberto2003@gmail.com>
2021-03-18 10:56:18 -0300

    Changed everything, a new code begins....

---
+++ src/modules/project.js
@@ -0,0 +1,12 @@
+
+const divProjects = document.querySelector('#projects');
+const divTasks = document.querySelector('#taks');
+const btnAddProjects = document.querySelector('#btnAddProject');
+const btnAddTask = document.querySelector('#btnAddTask');
+
+
+let projects = []; 
+let activeProject = 0;
+
+//addEventpara ambos os botões em direção ao form.js
+
